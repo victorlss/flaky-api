a3e2cea92d043033752deb3ee35e6948973df8d6
victorlss <contact@victorleonardo.com>
2021-08-18 21:52:10 -0300

    Add download-file pkg

---
+++ pkg/download-file/download.go
@@ -0,0 +1,43 @@
+package download_file
+
+import (
+	"github.com/hashicorp/go-retryablehttp"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+var (
+	fileDirectory = filepath.Join(os.TempDir())
+)
+
+const (
+	retries = 5
+)
+
+// Download a file from URL and store on file path
+func Download(url string, filename string) {
+	filePath := filepath.Join(fileDirectory, filename)
+
+	httpClient := retryablehttp.NewClient()
+	httpClient.RetryMax = retries
+
+	resp, err := httpClient.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	createDirectoryIfNotExists(filePath)
+	out, _ := os.Create(filePath)
+	_, _ = io.Copy(out, resp.Body)
+
+	_ = resp.Body.Close()
+	_ = out.Close()
+}
+
+func createDirectoryIfNotExists(filePath string) {
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		_ = os.Mkdir(filepath.Dir(filePath), 0755)
+	}
+}
---
+++ pkg/download-file/download_test.go
@@ -0,0 +1,35 @@
+package download_file
+
+import (
+	"io"
+	"mime/multipart"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestDownload(t *testing.T) {
+	server := httptest.NewServer(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			_, pw := io.Pipe()
+			r.Header.Add("Content-Type", multipart.NewWriter(pw).FormDataContentType())
+			w.WriteHeader(http.StatusOK)
+			_, _ = io.WriteString(w, "<svg></svg>")
+		}),
+	)
+
+	defer server.Close()
+	fileDirectory = os.TempDir()
+	fileName := "go_test.svg"
+	filePath := filepath.Join(fileDirectory, fileName)
+	_ = os.Remove(filePath)
+
+	Download(server.URL, fileName)
+
+	t.Log("should create a file named 'go_test.svg'")
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		t.Error("Expected file 'go_test.svg' to exist")
+	}
+}
